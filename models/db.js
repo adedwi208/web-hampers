8e13eb1802620019674c311e82a766232c532708
adedwi208 <adeagustus02@gmail.com>
2025-08-29 02:29:25 +0700

    pertama

--- models/db.js
+++ models/db.js
@@ -1,25 +1,18 @@
 const mysql = require('mysql2/promise');
-require("dotenv").config();
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
-
-async function testConnection() {
+async function connectDB() {
   try {
-    const connection = await pool.getConnection();
-    console.log("✅ Koneksi database berhasil!");
-    connection.release(); // Lepaskan koneksi kembali ke pool
+    const connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME
+    });
+    console.log("✅ DB Connected");
+    return connection;
   } catch (err) {
-    console.error("❌ Koneksi database gagal:", err);
-    // Berhenti jika koneksi gagal untuk mencegah error lebih lanjut
-    process.exit(1); 
+    console.error("❌ DB Error:", err);
   }
 }
 
-testConnection();
-
-module.exports = pool;
+connectDB();
