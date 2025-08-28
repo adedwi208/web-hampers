9bbb377e23bd3583e226f56d65f329d430826264
adedwi208 <adeagustus02@gmail.com>
2025-08-28 22:04:33 +0700

    fixed

--- models/db.js
+++ models/db.js
@@ -22,6 +22,7 @@ if (process.env.DATABASE_URL) {
     process.env.DB_PASS || "",
     {
       host: process.env.DB_HOST || "localhost",
+      port: process.env.DB_PORT || 3306,  // ✅ tambahin port
       dialect: "mysql",
       logging: false,
     }
