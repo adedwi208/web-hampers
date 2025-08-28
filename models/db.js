797bb23075bb0ad124b3b64d81a99165c62c7b39
adedwi208 <adeagustus02@gmail.com>
2025-08-29 03:11:40 +0700

    p

--- models/db.js
+++ models/db.js
@@ -1,32 +1,13 @@
-require('dotenv').config();
-const express = require('express');
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
-const app = express();
-const PORT = process.env.PORT || 8080;
-
-// koneksi MySQL
-async function connectDB() {
-  try {
-    const conn = await mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
-    });
-    console.log("✅ MySQL Connected!");
-    return conn;
-  } catch (err) {
-    console.error("❌ DB Connection Error:", err.message);
-  }
-}
-
-connectDB();
-
-app.get('/', (req, res) => {
-  res.send("Server sudah jalan tanpa Sequelize 🚀");
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+module.exports = pool;
